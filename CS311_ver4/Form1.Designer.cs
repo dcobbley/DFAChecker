53fbcbe0e0d91d8428b8de54306866ee9d830c74
dcobbley <djcobbles@gmail.com>
2014-12-09 23:58:42 -0800

    DFA checker for school
    Takes a DFA as input from a text file, input a string to test and checks
    for acceptance.

---
+++ CS311_ver4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_ImportDfa = new System.Windows.Forms.Button();
+            this.rtb_displayDfa = new System.Windows.Forms.RichTextBox();
+            this.labelDfa = new System.Windows.Forms.Label();
+            this.rtb_stringToCheck = new System.Windows.Forms.RichTextBox();
+            this.btn_submitString = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(374, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Davids Super Awesome DFA Checker";
+            // 
+            // btn_ImportDfa
+            // 
+            this.btn_ImportDfa.Location = new System.Drawing.Point(108, 72);
+            this.btn_ImportDfa.Name = "btn_ImportDfa";
+            this.btn_ImportDfa.Size = new System.Drawing.Size(75, 23);
+            this.btn_ImportDfa.TabIndex = 1;
+            this.btn_ImportDfa.Text = "Import DFA";
+            this.btn_ImportDfa.UseVisualStyleBackColor = true;
+            this.btn_ImportDfa.Click += new System.EventHandler(this.btn_ImportDfa_Click);
+            // 
+            // rtb_displayDfa
+            // 
+            this.rtb_displayDfa.Location = new System.Drawing.Point(108, 101);
+            this.rtb_displayDfa.Name = "rtb_displayDfa";
+            this.rtb_displayDfa.Size = new System.Drawing.Size(494, 266);
+            this.rtb_displayDfa.TabIndex = 2;
+            this.rtb_displayDfa.Text = "";
+            // 
+            // labelDfa
+            // 
+            this.labelDfa.AutoSize = true;
+            this.labelDfa.Location = new System.Drawing.Point(299, 82);
+            this.labelDfa.Name = "labelDfa";
+            this.labelDfa.Size = new System.Drawing.Size(56, 13);
+            this.labelDfa.TabIndex = 3;
+            this.labelDfa.Text = "Your DFA.";
+            this.labelDfa.Visible = false;
+            // 
+            // rtb_stringToCheck
+            // 
+            this.rtb_stringToCheck.Location = new System.Drawing.Point(108, 422);
+            this.rtb_stringToCheck.Name = "rtb_stringToCheck";
+            this.rtb_stringToCheck.Size = new System.Drawing.Size(297, 51);
+            this.rtb_stringToCheck.TabIndex = 4;
+            this.rtb_stringToCheck.Text = "Please enter a string to check";
+            this.rtb_stringToCheck.Click += new System.EventHandler(this.rtb_stringCheckClick);
+            // 
+            // btn_submitString
+            // 
+            this.btn_submitString.Location = new System.Drawing.Point(108, 480);
+            this.btn_submitString.Name = "btn_submitString";
+            this.btn_submitString.Size = new System.Drawing.Size(75, 23);
+            this.btn_submitString.TabIndex = 5;
+            this.btn_submitString.Text = "Submit";
+            this.btn_submitString.UseVisualStyleBackColor = true;
+            this.btn_submitString.Click += new System.EventHandler(this.btn_submitString_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 579);
+            this.Controls.Add(this.btn_submitString);
+            this.Controls.Add(this.rtb_stringToCheck);
+            this.Controls.Add(this.labelDfa);
+            this.Controls.Add(this.rtb_displayDfa);
+            this.Controls.Add(this.btn_ImportDfa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_ImportDfa;
+        private System.Windows.Forms.RichTextBox rtb_displayDfa;
+        private System.Windows.Forms.Label labelDfa;
+        private System.Windows.Forms.RichTextBox rtb_stringToCheck;
+        private System.Windows.Forms.Button btn_submitString;
+    }
+}
+
---
+++ CS311_ver4/Form1.cs
@@ -0,0 +1,198 @@
+﻿/* David Cobbley
+ * CS 311 
+ * 10-24-14
+ * DFA checker.
+ * Takes in a DFA from text in specific format, builds it, then checks a given string against it.
+ * 
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        //array of strings
+        string[] states, acceptingState, errorState;
+        string tempString, testString;
+        //multi dimensional array of strings for delta.
+        string[,] transitionStates;
+        char tempChar;
+        //acceptable characters in the language
+        char[] alphabet;
+        //ints most for initialization of string arrays
+        int numStates, numErrorStates, numAcceptingStates, numAlphabet, x, y, currentState;
+        bool errorFlag;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //If this button is pushed, import the DFA from text file in local directory.
+        private void btn_ImportDfa_Click(object sender, EventArgs e)
+        {
+            importDfa();
+        }
+        private void importDfa()
+        {
+            /*
+             * Grab input from text file.
+             * Input MUST be in format
+             * 
+             * number of states
+             * number of final states
+             * number of Error states
+             * number of characters in the alphabet
+             * states names, starting at q0
+             * name of accepting states
+             * error state name if any, else just skip
+             * characters in the alphabet
+             * starting at Q0, the transitions for each character in the alphabet
+             * 
+             */
+            using (StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "/dfa.txt"))
+            {
+                // set temp variables to the input for the stream reader
+                numStates = Convert.ToInt32(sr.ReadLine());
+                numAcceptingStates = Convert.ToInt32(sr.ReadLine());
+                numErrorStates = Convert.ToInt32(sr.ReadLine());
+                numAlphabet = Convert.ToInt32(sr.ReadLine());
+
+                //use variables to init array of strings.
+                states = new string[numStates];
+                acceptingState = new string[numAcceptingStates];
+                errorState = new string[numErrorStates];
+                alphabet = new char[numAlphabet + 1];
+
+                //grab all the of the states for each array of strings.
+                for (x = 0; x < numStates; x++)
+                    states[x] = sr.ReadLine();
+
+                for (x = 0; x < numAcceptingStates; x++)
+                    acceptingState[x] = sr.ReadLine();
+
+                for (x = 0; x < numErrorStates; x++)
+                    errorState[x] = sr.ReadLine();
+
+                for (x = 0; x < numAlphabet; x++)
+                    alphabet[x] = Convert.ToChar(sr.ReadLine());
+
+                //build delta.
+                transitionStates = new string[numStates, numAlphabet];
+
+                for (x = 0; x < numStates; ++x)
+                {
+                    for (y = 0; y < numAlphabet; ++y)
+                    {
+                        transitionStates[x, y] = sr.ReadLine();
+                    }
+                }
+            }
+            //Display the table
+            displayDfa();
+        }
+        private void displayDfa()
+        {
+            //work through multi dimensional array to display array.
+            rtb_displayDfa.Text = "state\t";
+            for (x = 0; x < numAlphabet; ++x)
+                rtb_displayDfa.Text+=alphabet[x] + "\t";
+
+            for (x = 0; x < numStates; x++)
+            {
+                rtb_displayDfa.Text += "\n";
+                rtb_displayDfa.Text+=states[x] + "\t";
+                for (y = 0; y < numAlphabet; y++)
+                    rtb_displayDfa.Text+=transitionStates[x, y] + "\t";
+            }
+
+            labelDfa.Visible = true;
+        }
+        //if this button is pushed, call checkString function.
+        private void btn_submitString_Click(object sender, EventArgs e)
+        {
+            if(rtb_stringToCheck.Text!=""&&rtb_stringToCheck.Text!="Please enter a string to check")
+                checkString();
+        }
+        private void checkString()
+        {
+            currentState = 0;
+            errorFlag = false;
+
+            testString = rtb_stringToCheck.Text.ToString();
+
+
+            for (x = 0; x < testString.Length; ++x)
+            {
+                tempChar = testString[x];
+                //move across the x axis of the table
+                for (y = 0; y < numAlphabet; ++y)
+                {
+                    if (alphabet[y] == tempChar)
+                        break;
+                }
+                tempString = transitionStates[currentState, y];
+
+                //Move across the y-axis of the table
+                for (int k = 0; k < numStates; k++)
+                {
+                    if (states[k] == tempString)
+                    {
+                        currentState = k;
+                        break;
+                    }
+                }
+
+                //check if error state
+                for (y = 0; y < numErrorStates; y++)
+                {
+                    if (states[currentState] == errorState[y])
+                    {
+                        rtb_stringToCheck.Text+="\n String rejected";
+                        rtb_stringToCheck.ForeColor = Color.Red;
+                        errorFlag = true;
+                        break;
+                    }
+                }
+                if (errorFlag)
+                    break;
+
+            }
+
+            for (x = 0; x < numAcceptingStates; x++)
+            {
+                //check to make sure you end up in the accepting state
+                if (states[currentState] == acceptingState[x])
+                {
+                    rtb_stringToCheck.Text += "\n String Accepted";
+                    rtb_stringToCheck.ForeColor = Color.Green;
+                }
+                else
+                {
+                    rtb_stringToCheck.Text += "\n String rejected";
+                    rtb_stringToCheck.ForeColor = Color.Red;
+                }
+            }
+        }
+        //reset rich text box.
+        private void rtb_stringCheckClick(object sender, EventArgs e)
+        {
+            rtb_stringToCheck.ForeColor = Color.Black;
+            rtb_stringToCheck.Text = "";
+        }
+    }
+}
